3ebff1afcc57609c6ed6995ff78596139b056651
JoyAnneW <joyawilliams@gmail.com>
2022-05-25 15:25:22 +0200

    installed bcrypt

--- server.js
+++ server.js
@@ -16,4 +16,9 @@ const posts = [
 app.get("/posts", (req, res) => {
 	res.json(posts);
 });
+
+// to authenticate route so not everyone can get the posts
+app.get("/login", (req, res) => {
+	// Authenticate User
+});
 app.listen(3000);
